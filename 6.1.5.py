521eca390be310728c2e94c01e5db6690bfae899
Pablo Ventura <pablogventura@gmail.com>
2017-06-08 12:46:24 -0300

    6.1.5

---
+++ 6.1.5.py
@@ -0,0 +1,46 @@
+from __future__ import division
+import numpy as np
+
+qcero=np.array([(1,),(0,)])
+quno =np.array([(0,),(1,)])
+
+hadamard = np.array([(1/np.sqrt(2),1/np.sqrt(2)),
+                     (1/np.sqrt(2),-1/np.sqrt(2))])
+                     
+def estado(q1,q2):
+    return np.kron(q1,q2)
+
+def phi2(uf):
+    return np.matmul(uf,
+        np.matmul(np.kron(hadamard,hadamard),
+        estado(qcero,quno)))
+    
+
+
+#funcion que intercambia
+uf=np.array([(0,1,0,0),
+            (1,0,0,0),
+            (0,0,1,0),
+            (0,0,0,1)])
+print phi2(uf)
+
+# print np.kron(((-qcero+quno)/np.sqrt(2)), ((qcero-quno)/np.sqrt(2))) # resultado que da en el libro
+
+#funcion constante 0
+uf=np.array([(1,0,0,0),
+             (0,1,0,0),
+             (0,0,1,0),
+             (0,0,0,1)])
+print phi2(uf)
+#funcion constante 1
+uf=np.array([(0,1,0,0),
+            (1,0,0,0),
+            (0,0,0,1),
+            (0,0,1,0)])
+print phi2(uf)
+#funcion identidad
+uf=np.array([(1,0,0,0),
+            (0,1,0,0),
+            (0,0,0,1),
+            (0,0,1,0)])
+print phi2(uf)
